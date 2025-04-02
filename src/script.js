e0c23391f8933463747137ff3417a42f327b1f0b
Zwe <zewlinnmg@gmail.com>
2025-04-02 18:02:24 +0700

    day1

---
+++ src/script.js
@@ -0,0 +1,80 @@
+import * as THREE from 'three'
+import { rotate } from 'three/tsl'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Object
+ */
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+// mesh.position.z = 1
+// mesh.position.x = -0.8
+// mesh.position.z = 0.7
+
+// // set method
+// mesh.position.set(1, -0.5, -0.5)
+// console.log(mesh.position.length());
+
+//group
+const group = new THREE.Group()
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+cube1.position.set(1, -0.5, -0.5)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+cube2.position.set(-1, 0.5, 0.5)
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+
+group.add(cube3)    
+
+group.position.y = 1
+
+//AxesHelper
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+camera.position.y = 1
+camera.position.x = 1
+scene.add(camera)
+
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
