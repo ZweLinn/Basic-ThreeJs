2084dce342aaeec200fca397e66dde4fcdce3bbd
Zwe <zewlinnmg@gmail.com>
2025-04-07 12:18:44 +0700

    day3

--- src/RotatingCamera.js
+++ src/RotatingCamera.js
@@ -1,5 +1,6 @@
 
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
 const canvas = document.querySelector('canvas.webgl');
 
@@ -13,8 +14,9 @@ scene.add(mesh1);
 
 //sizes
 const sizes = {
-  width: 1250,
-  height: 550,
+  width: window.innerWidth,
+  height: window.innerHeight,
+
 };
 
 const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height,0.1,100)
@@ -24,9 +26,26 @@ camera.position.y = 1;
 camera.position.x = 1;
 camera.lookAt(mesh1.position)
 
+//controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true;
+
 
 const renderer = new THREE.WebGLRenderer({ canvas : canvas });
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+//double click to fullscree
+window.addEventListener('dblclick', () => {
+  if (!document.fullscreenElement) {
+    canvas.requestFullscreen();
+  }
+  else {
+    document.exitFullscreen();
+  }
+})
+
+
 
 
 //clock
@@ -46,11 +65,13 @@ window.addEventListener('mousemove', (event) => {
 //animation
 const animate = () => {
    
-    const elapsedTime = clock.getElapsedTime();
-    camera.position.x = Math.sin(mouse.x * Math.PI) * 2;
-    camera.position.z = Math.cos(mouse.x * Math.PI) * 2;
-    camera.position.y = mouse.y * 2;
-    camera.lookAt(mesh1.position)
+    // const elapsedTime = clock.getElapsedTime();
+    // camera.position.x = Math.sin(mouse.x * Math.PI) * 2;
+    // camera.position.z = Math.cos(mouse.x * Math.PI) * 2;
+    // camera.position.y = mouse.y * 2;
+    // camera.lookAt(mesh1.position)
+    controls.update()
+
   
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
