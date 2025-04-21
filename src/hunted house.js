05e06a7359974093f21870c620ff0a711d1a86a1
Zwe <zewlinnmg@gmail.com>
2025-04-21 16:44:04 +0700

    day12

--- src/hunted house.js
+++ src/hunted house.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { Timer } from 'three/addons/misc/Timer.js'
+import { Sky } from 'three/addons/objects/Sky.js'
 import GUI from 'lil-gui'
 
 /**
@@ -228,17 +229,53 @@ for (let i = 0; i < 30; i++) {
 
 }
 
+
+
 /**
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
+const ambientLight = new THREE.AmbientLight('#86cdff', 0.25)
 scene.add(ambientLight)
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight('#ffffff', 1.5)
+const directionalLight = new THREE.DirectionalLight('#86cdff', .8) 
 directionalLight.position.set(3, 2, -8)
+
 scene.add(directionalLight)
+
+//door light
+const doorLight = new THREE.PointLight('#ff7d46', 5)
+doorLight.position.set(0, 2, 2.5)
+house.add(doorLight)
+
+//ghost
+const ghostLight = new THREE.PointLight('#8800ff', 6, 1, 2);
+scene.add(ghostLight);
+//ghost
+
+//shadow
+directionalLight.castShadow = true
+//mapping
+directionalLight.shadow.mapSize.width = 256
+directionalLight.shadow.mapSize.height = 256
+directionalLight.shadow.camera.top = 10
+directionalLight.shadow.camera.right = 10
+directionalLight.shadow.camera.bottom = -10
+directionalLight.shadow.camera.left = -10
+directionalLight.shadow.camera.far = 20
+directionalLight.shadow.camera.near = 1
+
+
+wall.castShadow = true
+wall.receiveShadow = true
+roof.castShadow = true
+floor.receiveShadow = true
+
+for(const grave of graves.children){
+    grave.castShadow = true
+    grave.receiveShadow = true
+}
 
 /**
  * Sizes
@@ -254,7 +291,8 @@ const sizes = {
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
 window.addEventListener('resize', () => {
     // Update sizes
@@ -270,6 +308,19 @@ window.addEventListener('resize', () => {
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
+//sky
+const sky = new Sky()
+sky.material.uniforms['turbidity'].value = 10
+sky.material.uniforms['rayleigh'].value = 10
+sky.material.uniforms['mieCoefficient'].value = 0.1
+sky.material.uniforms['mieDirectionalG'].value = 0.95
+sky.material.uniforms['sunPosition'].value.set(0.3, -0.038, -0.95)
+
+sky.scale.set(200,200,200)
+scene.add(sky)
+
+//fog
+scene.fog = new THREE.FogExp2("#02343f", 0.1)
 /**
  * Camera
  */
